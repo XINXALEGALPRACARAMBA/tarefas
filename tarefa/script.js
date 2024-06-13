605cf52f2f0bb22254289b491736470ac6e0d6fa
XINXALEGALPRACARAMBA <129397421+XINXALEGALPRACARAMBA@users.noreply.github.com>
2024-06-13 12:13:50 -0300

    Update script.js

--- tarefa/script.js
+++ tarefa/script.js
@@ -1,11 +1,5 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-const Name_alunos0 = ["ANA CAROLINA PONTAROLO CARVALHO", 
+const Name_alunos0 = [
+    "ANA CAROLINA PONTAROLO CARVALHO", 
     "ANA HELOISE MONTEIRO DA SILVA", 
     "ANA LUIZA DA SILVA NOGUEIRA", 
     "ANDRE CARDIN SOARES",
@@ -35,68 +29,24 @@ const Name_alunos0 = ["ANA CAROLINA PONTAROLO CARVALHO",
     "VICTOR DE PAULA",
     "VINICIUS BORTOLI",
     "WILLIAN NOVAES BITTENCOURT"
- ];
+];
 const Number_alunos0 = [1, 2, 3, 4,
-           5, 6, 7, 8,
-           9, 10, 11, 12,
-           13, 14, 15, 16,
-           17, 18, 19, 21,
-           22, 23, 24, 26,
-           27, 28, 34, 30,  
-           31, 32, 33   ];
+    5, 6, 7, 8,
+    9, 10, 11, 12,
+    13, 14, 15, 16,
+    17, 18, 19, 21,
+    22, 23, 24, 26,
+    27, 28, 34, 30,  
+    31, 32, 33   
+];
 
-const Name_alunos = ["ANA CAROLINA PONTAROLO CARVALHO", 
-    "ANA HELOISE MONTEIRO DA SILVA", 
-    "ANA LUIZA DA SILVA NOGUEIRA", 
-    "ANDRE CARDIN SOARES",
-    "BEATRIZ BRANDT VIANA",
-    "BHIANCA MULDENBERGER PINTO",
-    "CAUÃ JUN KESHINO",
-    "ELIÃ GABRIEL VIEIRA",
-    "ENZO SAMUEL LISBOA ZORDENUNES",
-    "GIOVANNA CALDEIRA",
-    "HENRIQUE DUARTE DOS SANTOS",
-    "HENRIQUE EMILIO SILVA KRUGER",
-    "IAGO DANIEL CALIXTO",
-    "IASMIN APARECIDA DA SILVA PEREIRA",
-    "ISADORA CRISTINA SILVEIRA",
-    "JHENYFER GUIMARÃES DE LARA",
-    "JOAO FRANCISCO KUJIV",
-    "JOÃO HENRIQUE MORAIS DE ASSIS",
-    "LORENZO HENRIQUE BENIN",
-    "LUIZ FELIPE DO ROSARIO DE PAULA",
-    "LUIZ GUSTAVO BRASIL",
-    "MARIA JULIA AMORIM DA SILVA",
-    "MARIA LUYZA MENDES DA SILVA LEAL",
-    "PAOLA RAFAELA DOS SANTOS AMARAL",
-    "PEDRO HENRIQUE LOPES DE LARA",
-    "RENATO GABRIEL VIANA STRAUBE",
-    "RICHARD CARRAR0",
-    "VICTOR DE PAULA",
-    "VINICIUS BORTOLI",
-    "WILLIAN NOVAES BITTENCOURT"
- ];
-const Number_alunos = [1, 2, 3, 4,
-           5, 6, 7, 8,
-           9, 10, 11, 12,
-           13, 14, 15, 16,
-           17, 18, 19, 21,
-           22, 23, 24, 26,
-           27, 28, 29, 30,  
-           31, 32, 33   ];
-
-
-const listaDeAlunosENumeros = [Name_alunos0, Number_alunos0];
-const listaDeAlunosENumeros1 = [Name_alunos, Number_alunos];
-
-
-rl.question('Qual lista de alunos você deseja visualizar? Digite 0 para a primeira lista ou 1 para a segunda lista: ', (escolhaLista) => {
-    const listaSelecionada = escolhaLista === '0' ? listaDeAlunosENumeros : listaDeAlunosENumeros1;
-
-    for (let i = 0; i < listaSelecionada[0].length; i++) {
-        console.log(`Nome: ${listaSelecionada[0][i]} Número: ${listaSelecionada[1][i]}.`);
+const novaListaDeChamada = [];
+for (let i = 0; i < Name_alunos0.length; i++) {
+    if (Number_alunos0[i] !== 34) {
+        novaListaDeChamada.push({ numero: Number_alunos0[i], nome: Name_alunos0[i] });
+    } else {
+        novaListaDeChamada.push({ numero: 29, nome: "RICHARD CARRARO" });
     }
+}
 
-    rl.close();
-});
-
+console.log(novaListaDeChamada);
